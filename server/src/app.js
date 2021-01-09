e5a25f7146b01914139e0ae2b9f0cd488774a8b6
leonidk <e_leokim@yahoo.com>
2021-01-08 18:35:39 -0800

    server: set default port 4001

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const randomColor = require('randomcolor')
 const uuid = require('uuid')
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4001
 
 const app = express()
 server = app.listen(port)
